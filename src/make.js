351c79964ffebdee7942277d9c75d96a9ed106d9
Lesechka <littlecare@mail.ru>
2018-10-28 19:28:57 +0300

    Add: function make

---
+++ src/make.js
@@ -0,0 +1,15 @@
+function sum(a, b) {
+    return a + b;
+}
+function make(...arguments) {
+    const arrOfArgs = [];
+    arrOfArgs.push(...arguments);
+    return function result (...args) {
+        if (typeof args[0] === 'function') {
+            const func = args[0];
+            return arrOfArgs.reduce((acc, num) => func(acc, num));
+        }
+        arrOfArgs.push(...args);
+        return result;
+    };
+}
